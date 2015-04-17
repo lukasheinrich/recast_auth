f6c20a37c9a3dc3b66e0d57e3e649f66dc3b51d4
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-04-17 15:11:06 +0900

    check permission

--- getmyproxy.sh
+++ getmyproxy.sh
@@ -1,16 +1,17 @@
 #!/bin/zsh
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 scriptdir=$(dirname $0)
 
-if ! type "voms-proxy-init" > /dev/null;then
-localSetupEmi
-fi
 export X509_USER_CERT=$scriptdir/host.cert
 export X509_USER_KEY=$scriptdir/privkey.pem
 
 if [ ! $(stat --format=%a "$X509_USER_KEY") -eq 400 ];then
   echo "warning: permissions on private key: $X509_USER_KEY are not correct. should be 400";
   exit 1
+fi
+
+if ! type "voms-proxy-init" > /dev/null;then
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+localSetupEmi
 fi
   
 
