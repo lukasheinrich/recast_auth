1f06057d77ce927a6c889895576019ec63c1ba6c
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-04-17 14:24:24 +0900

    credential name by hostname

--- getmyproxy.sh
+++ getmyproxy.sh
@@ -5,5 +5,5 @@ scriptdir=$(dirname $0)
 if ! type "voms-proxy-init" > /dev/null;then
 localSetupEmi
 fi
-X509_USER_CERT=$scriptdir/host.cert X509_USER_KEY=$scriptdir/privkey.pem myproxy-logon -l lheinric -k recast_proxy -n --voms atlas
+X509_USER_CERT=$scriptdir/host.cert X509_USER_KEY=$scriptdir/privkey.pem myproxy-logon -l lheinric -k "recast_$(hostname)" -n --voms atlas
 voms-proxy-info --all
